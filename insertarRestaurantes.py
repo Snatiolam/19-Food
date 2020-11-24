664b8e8270867441d2b0a82262983f09cb5894d3
Snatiolam <santiago.alzate1@hotmail.com>
2020-11-24 05:47:04 -0500

    Insert de horas

--- insertarRestaurantes.py
+++ insertarRestaurantes.py
@@ -2,7 +2,7 @@ from app import *
 
 #Creacion cuenta admin 
 user = Usuarios.query.filter_by(username="admin").first()
-new_restaurant = Restaurantes(id_user=user.id, img_url="https://10619-2.s.cdn12.com/rests/original/327_356523056.jpg", tipo="RAP",nombre="Mandingas",hor_abierto="9:00",hor_cierre="10:00",personas=12) 
+new_restaurant = Restaurantes(id_user=user.id, img_url="https://10619-2.s.cdn12.com/rests/original/327_356523056.jpg", tipo="RAP",nombre="Mandingas",hor_abierto="09:00",hor_cierre="10:00",personas=12) 
 comp_rest = Restaurantes.query.filter_by(nombre="Mandingas").first()
 if comp_rest is not None:
     pass
@@ -10,7 +10,7 @@ else:
     db.session.add(new_restaurant)
     db.session.commit()
 
-new_restaurant = Restaurantes(id_user=user.id, img_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvdG_k7keQLh0vrkrjPI6h99uQAqJOfoScsQ&usqp=CAU", tipo="PER",nombre="Sabor Peru",hor_abierto="9:00",hor_cierre="10:00",personas=12) 
+new_restaurant = Restaurantes(id_user=user.id, img_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvdG_k7keQLh0vrkrjPI6h99uQAqJOfoScsQ&usqp=CAU", tipo="PER",nombre="Sabor Peru",hor_abierto="09:00",hor_cierre="10:00",personas=12) 
 comp_rest = Restaurantes.query.filter_by(nombre="Sabor Peru").first()
 if comp_rest is not None:
     pass
@@ -18,7 +18,7 @@ else:
     db.session.add(new_restaurant)
     db.session.commit()
 
-new_restaurant = Restaurantes(id_user=user.id, img_url="https://img.freepik.com/vector-gratis/logos-restaurantes-mexicanos-contorno_23-2147546379.jpg?size=338&ext=jpg", tipo="MEX",nombre="Mister Taco",hor_abierto="9:00",hor_cierre="10:00",personas=12) 
+new_restaurant = Restaurantes(id_user=user.id, img_url="https://img.freepik.com/vector-gratis/logos-restaurantes-mexicanos-contorno_23-2147546379.jpg?size=338&ext=jpg", tipo="MEX",nombre="Mister Taco",hor_abierto="09:00",hor_cierre="10:00",personas=12) 
 comp_rest = Restaurantes.query.filter_by(nombre="Mister Taco").first()
 if comp_rest is not None:
     pass
@@ -32,7 +32,7 @@ Usuario salzatec1
 
 user = Usuarios.query.filter_by(username="salzatec1").first()
 
-new_restaurant = Restaurantes(id_user=user.id, img_url="https://pbs.twimg.com/media/D26_z5ZX4AEGGli.jpg", tipo="COL",nombre="Mondongo's",hor_abierto="9:00",hor_cierre="10:00",personas=12) 
+new_restaurant = Restaurantes(id_user=user.id, img_url="https://pbs.twimg.com/media/D26_z5ZX4AEGGli.jpg", tipo="COL",nombre="Mondongo's",hor_abierto="09:00",hor_cierre="10:00",personas=12) 
 comp_rest = Restaurantes.query.filter_by(nombre="Mondongo's").first()
 if comp_rest is not None:
     pass
@@ -40,7 +40,7 @@ else:
     db.session.add(new_restaurant)
     db.session.commit()
 
-new_restaurant = Restaurantes(id_user=user.id, img_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVoxUWnlaqnEwo4gwXzHxTodSeOeosGTgfQA&usqp=CAU", tipo="ALT",nombre="Liguria artesanal",hor_abierto="9:00",hor_cierre="10:00",personas=12) 
+new_restaurant = Restaurantes(id_user=user.id, img_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVoxUWnlaqnEwo4gwXzHxTodSeOeosGTgfQA&usqp=CAU", tipo="ALT",nombre="Liguria artesanal",hor_abierto="09:00",hor_cierre="10:00",personas=12) 
 comp_rest = Restaurantes.query.filter_by(nombre="Liguria artesanal").first()
 if comp_rest is not None:
     pass
@@ -48,7 +48,7 @@ else:
     db.session.add(new_restaurant)
     db.session.commit()
 
-new_restaurant = Restaurantes(id_user=user.id, img_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQgXux0VzOt5zIpJGMuyaaDbtCYMUpD-KZeQ&usqp=CAU", tipo="ITA",nombre="Ragazzi",hor_abierto="9:00",hor_cierre="10:00",personas=12) 
+new_restaurant = Restaurantes(id_user=user.id, img_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQgXux0VzOt5zIpJGMuyaaDbtCYMUpD-KZeQ&usqp=CAU", tipo="ITA",nombre="Ragazzi",hor_abierto="09:00",hor_cierre="10:00",personas=12) 
 comp_rest = Restaurantes.query.filter_by(nombre="Ragazzi").first()
 if comp_rest is not None:
     pass
@@ -62,7 +62,7 @@ Usuario surregog
 
 user = Usuarios.query.filter_by(username="surregog").first()
 
-new_restaurant = Restaurantes(id_user=user.id, img_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkapkVzPf_MGxrA2LSgjwu_6hh71nm42ZbIw&usqp=CAU", tipo="MEX",nombre="El chacho",hor_abierto="9:00",hor_cierre="10:00",personas=12) 
+new_restaurant = Restaurantes(id_user=user.id, img_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkapkVzPf_MGxrA2LSgjwu_6hh71nm42ZbIw&usqp=CAU", tipo="MEX",nombre="El chacho",hor_abierto="09:00",hor_cierre="10:00",personas=12) 
 comp_rest = Restaurantes.query.filter_by(nombre="El chacho").first()
 if comp_rest is not None:
     pass
@@ -70,7 +70,7 @@ else:
     db.session.add(new_restaurant)
     db.session.commit()
 
-new_restaurant = Restaurantes(id_user=user.id, img_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHFtW8DxZdO2lyRLarotEQA9e9BckptlSpHg&usqp=CAU", tipo="PER",nombre="Miraflores",hor_abierto="9:00",hor_cierre="10:00",personas=12) 
+new_restaurant = Restaurantes(id_user=user.id, img_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHFtW8DxZdO2lyRLarotEQA9e9BckptlSpHg&usqp=CAU", tipo="PER",nombre="Miraflores",hor_abierto="09:00",hor_cierre="10:00",personas=12) 
 comp_rest = Restaurantes.query.filter_by(nombre="Miraflores").first()
 if comp_rest is not None:
     pass
@@ -78,7 +78,7 @@ else:
     db.session.add(new_restaurant)
     db.session.commit()
 
-new_restaurant = Restaurantes(id_user=user.id, img_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzBvQZv2QFuNasiNMHI3Z20Th9g2SNR4xmCg&usqp=CAU", tipo="RAP",nombre="Los perritos",hor_abierto="9:00",hor_cierre="10:00",personas=12) 
+new_restaurant = Restaurantes(id_user=user.id, img_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzBvQZv2QFuNasiNMHI3Z20Th9g2SNR4xmCg&usqp=CAU", tipo="RAP",nombre="Los perritos",hor_abierto="09:00",hor_cierre="10:00",personas=12) 
 comp_rest = Restaurantes.query.filter_by(nombre="Los perritos").first()
 if comp_rest is not None:
     pass
